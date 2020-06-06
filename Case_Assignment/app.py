db5bc4aec42d554dd706f4d183f11dcfae2765da
clemiblac <48965151+clemiblac@users.noreply.github.com>
2020-06-06 17:44:38 -0400

    creating app

--- Case_Assignment/app.py
+++ Case_Assignment/app.py
@@ -6,61 +6,52 @@ Created on Thu Jun  4 15:48:04 2020
 """
 
 import numpy as np
-
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 import pandas as pd
 
 from flask import Flask, jsonify
 
-#%%
-
-#################################################
-# Database Setup
-#################################################
-engine = create_engine("sqlite:///titanic.sqlite")
 
 #%%
+### Flask Setup
 
-#################################################
-# Flask Setup
-#################################################
 app = Flask(__name__)
 
 #%%
-#################################################
 # Flask Routes
-#################################################
 #%%
 @app.route("/")
 def welcome():
     """List all available api routes."""
     return (
+        f"Welcome to the home page with the list of available routes<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
     )
 
+if __name__ == '__main__':
+    app.run(debug=True)
 #%%
-@app.route("/api/v1.0/names")
-def names():
-    
-    results = pd.read_sql('SELECT * FROM passenger', engine)
+# Database Setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+#%%
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+        
+    results = pd.read_sql('SELECT * FROM measurement', engine)
 
-    results_json = results[['name','age']].to_json(orient='records') 
+    results_json = results[['station','prcp']].to_json(orient='records') 
 
     return results_json
 
+ #Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
 
-#%%
-@app.route('/api/v1.0/passengers-sql')
-def passengers_sql():
-
-    results = pd.read_sql('SELECT * FROM passenger', engine)
-
-    results_json = results.to_json(orient='records') 
-
-    return results_json
+ #Return the JSON representation of your dictionary.
 
 #%%
 if __name__ == '__main__':
