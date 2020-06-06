2ffbee64dda030b2ddccb0fbbda56107d47645fc
clemiblac <48965151+clemiblac@users.noreply.github.com>
2020-06-06 18:50:07 -0400

    working on app

--- Case_Assignment/app.py
+++ Case_Assignment/app.py
@@ -4,15 +4,11 @@ Created on Thu Jun  4 15:48:04 2020
 
 @author: clemi
 """
-
+from flask import Flask, jsonify
 import numpy as np
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 import pandas as pd
-
-from flask import Flask, jsonify
-
-
 #%%
 ### Flask Setup
 
@@ -24,8 +20,8 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     """List all available api routes."""
+    """Welcome to the home page with the list of available routes"""
     return (
-        f"Welcome to the home page with the list of available routes<br/>"
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
@@ -33,25 +29,18 @@ def welcome():
         f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end><br/>"
     )
-
-if __name__ == '__main__':
-    app.run(debug=True)
 #%%
-# Database Setup
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-#%%
+database_path="Resources/hawaii.sqlite"
+engine = create_engine(f"sqlite:///{database_path}")
+conn=engine.connect()
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-        
-    results = pd.read_sql('SELECT * FROM measurement', engine)
-
-    results_json = results[['station','prcp']].to_json(orient='records') 
-
-    return results_json
-
- #Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
-
- #Return the JSON representation of your dictionary.
+    data = pd.read_sql('SELECT * FROM measurement', engine)
+    return(data.head())
+    
+#Convert the query results to a dictionary using `date` as the key and `prcp` as the value
+#Return the JSON representation of your dictionary.
+            
 
 #%%
 if __name__ == '__main__':
